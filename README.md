cf1c4472ccb4a08c24ee289d6c2592252c4cafa1
DELL <sangyeop0529@gmail.com>
2025-05-15 02:16:51 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+1. npm create vite@lastest ./
+2. npm i react-router-dom
---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Diary from "./pages/Diary";
+import Edit from "./pages/Edit";
+import Notfound from "./pages/Notfound";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary/:id" element={<Diary />} />
+        <Route path="/edit/:id" element={<Edit />} />
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
