27ac29187fef8a0754406840305e9505d084a2ff
DELL <sangyeop0529@gmail.com>
2025-05-15 17:26:36 +0900

    part08 : Diary Component

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,29 +1,16 @@
-import { useContext, useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { DiaryDispatchContext, DiaryStateContext } from "../App";
 import Header from "../components/Header";
 import Button from "../components/Button";
 import Editor from "../components/Editor";
+import { useContext } from "react";
+import { DiaryDispatchContext, DiaryStateContext } from "../App";
+import useDiary from "../hooks/useDiary";
 
 const Edit = () => {
   const params = useParams();
   const nav = useNavigate();
-  const data = useContext(DiaryStateContext);
   const { onUpdate, onDelete } = useContext(DiaryDispatchContext);
-  const [curDiaryItem, setCurDiaryItem] = useState();
-
-  useEffect(() => {
-    const currentDiaryItem = data.find(
-      (item) => String(item.id) === String(params.id)
-    );
-
-    if (!currentDiaryItem) {
-      window.alert("존재하지 않는 일기입니다.");
-      nav("/", { replace: true });
-    }
-
-    setCurDiaryItem(currentDiaryItem);
-  }, [params.id]);
+  const curDiaryItem = useDiary(params.id);
 
   const onClickDelete = () => {
     if (window.confirm("일기를 정말 삭제할까요? 다시 복구되지 않아요!")) {
